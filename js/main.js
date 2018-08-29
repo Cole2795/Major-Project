aa9fab1484354ad0fa500efac7ff4d26e8c81080
Cole2795 <42801104+Cole2795@users.noreply.github.com>
2018-08-29 11:17:01 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//$(function(){
+//    $(document).on('submit', '#chatform', function){
+//      var text = $.trim($("#$text").val());
+//      var user_id = $.trim($("#$user_id").val());
+//      
+//      if(text != "" && name != ""){
+//          $.post('message.php',{text: text, user_id: user_id},function(data){
+//              $(".chatMessage").append(data);
+//          });
+//      }else{
+//          alert("DATA MISSING");
+//      }
+//    }
+//});
